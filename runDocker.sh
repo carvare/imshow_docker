9397a9df091827c21305a05e2a0c7df3161addba
carvare <37734288+carvare@users.noreply.github.com>
2022-10-19 09:40:48 +0200

    Add files via upload

--- runDocker.sh
+++ runDocker.sh
@@ -2,5 +2,7 @@ xhost +local:docker
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-docker run -m 8GB -it --rm -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -v ${PWD}:/src  -it jerinka/opencv:1
+# original
+# docker run -m 8GB -it --rm -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -v ${PWD}:/src  -it jerinka/opencv:1
+docker run -m 8GB -it --rm --device=/dev/video0:/dev/video0 --network=minet --name=minet_dns  -p 6666:6666 -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -v ${PWD}:/src  -it jerinka/opencv:1
 xhost -local:docker
